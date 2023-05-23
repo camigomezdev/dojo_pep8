ecdc599d3bb4d7644c5d519918426c9b110f37ae
Cami Gomez <camigomez@hugeinc.com>
2023-05-23 16:37:41 -0500

    Codebreaker game init

---
+++ main.py
@@ -0,0 +1,18 @@
+from codebreaker import Codebreaker
+
+intentos_totales = 10
+codebreaker = Codebreaker()
+
+intento = 0
+
+print('Jugar Codebreaker!')
+
+while intento != intentos_totales:
+   number = input('Numero:');
+   resolve = codebreaker.adivinar(number)
+   print(resolve)
+   if resolve == True:
+      print('You win!!')
+      break
+
+

1a3eaf9db8597e62b1175e7467dd3cf6b44c60d2
devAbreu <yolfrank.abreu@gmail.com>
2023-05-24 11:19:03 -0500

    :bug: fixed logic bug

--- codebreaker.py
+++ codebreaker.py
@@ -1,35 +1,43 @@
 true_number = "1010"
 
-
 class CodeBreaker:
+    """
+    CodeBreaker class represents a code-breaking game where the player guesses a number.
+    """
 
     def guess(self, number=None):
+        """
+        Guesses the given number and returns the result.
+
+        Args:
+            number (str): The number to guess.
+
+        Returns:
+            str or bool: The result of the guess. Returns 'Number is not defined' if true_number is not defined,
+                        'error' if the number is not provided or has an incorrect length,
+                        True if the guess matches the true_number,
+                        or a combination of 'X' and '_' characters indicating the correct and incorrect positions.
+
+        """
         if not true_number:
             return 'Number is not defined'
-        
-        if not number or len(number) != 4:
+
+        if number is None or len(number) != 4:
             return "error"
 
         if number == true_number:
             return True
 
         resultX = ''
-        result = ''
-        array_number = []
-
-        for x in len(number):
-            if (array_number[number[x]]):
-                return 'error'
-
-            array_number[number[x]] = True
+        result_ = ''
 
         number = list(number)
 
-        for index, x in number:
+        for index, x in enumerate(number):
             if true_number[index] == number[index]:
                 resultX += 'X'
-
             elif x in true_number:
-                result = '_'
+                result_ = '_'
 
-        return resultX + result
+        return resultX + result_
+

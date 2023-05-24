42b7003c6d7b0bc2a63c081119ffa532f61f74dd
devAbreu <yolfrank.abreu@gmail.com>
2023-05-23 21:49:29 -0500

    :art: applied autopep8 to code format

--- codebreaker.py
+++ codebreaker.py
@@ -1,34 +1,35 @@
-trueNumber = "1010";
+trueNumber = "1010"
 
-class Codebreaker:
-    
-  def adivinar(self, numero=None):
-    if trueNumber == '':
-      return 'Number is not defined'
 
-    if numero is None or len(numero) != 4 or 'e' not in list(numero):
-      return "error"
-    
-    if numero == trueNumber:
-      return True
+class CodeBreaker:
 
-    resultadoX  = ''
-    resultado_  = ''
-    arrayNumber = []
-    
-    for x in len(numero):
-      if(arrayNumber[numero[x]] == True):
-        return 'error'
+    def guess(self, number=None):
+        if trueNumber == '':
+            return 'Number is not defined'
 
-      arrayNumber[numero[x]] = True
-    
-    numero = list(numero)
+        if number is None or len(number) != 4 or 'e' not in list(number):
+            return "error"
 
-    for index, x in numero:
-      if trueNumber[index] == numero[index]:
-        resultadoX+='X'
+        if number == trueNumber:
+            return True
 
-      elif x in trueNumber:
-        resultado_='_'
+        resultadoX = ''
+        resultado_ = ''
+        arrayNumber = []
 
-    return resultadoX+resultado_
+        for x in len(number):
+            if (arrayNumber[number[x]]):
+                return 'error'
+
+            arrayNumber[number[x]] = True
+
+        number = list(number)
+
+        for index, x in number:
+            if trueNumber[index] == number[index]:
+                resultadoX += 'X'
+
+            elif x in trueNumber:
+                resultado_ = '_'
+
+        return resultadoX + resultado_

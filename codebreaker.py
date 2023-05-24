00e0965aac018f638f78901e5fb70d7360bfbc5a
Eduardo Chapa <eduardo.chapa@gmail.com>
2023-05-23 23:28:36 -0600

    Improvements

--- codebreaker.py
+++ codebreaker.py
@@ -1,40 +1,51 @@
 """
-    This module defines the Codebreaker class, which has a single method
-    called "guess"
+    This module defines the Codebreaker class. It imports the
+    random Python module to generate digits randomly for the "right_number"
+    variable.
 """
-
-RIGHT_NUMBER = "1023"
+import random
 
 
 class Codebreaker:
-    """
-        This class defines the Codebreaker object, and contains only one method
-        named "guess"
-    """
+    """ This class defines the Codebreaker object. """
+
+    def __init__(self):
+        """
+            This method initializes the "right_number" variable to a string
+            converted from a 4 digit random number without repeated digits.
+        """
+        self.right_number = str(''.join(random.sample("0123456789", 4)))
 
     def guess(self, number=None):
         """
             This method takes two parameters, self and number (with a default
             value of None). It will test if the "number" entered by the user
-            matches the "RIGHT_NUMBER" constant, and if not, return an "X" for
-            each number guessed correctly and in the right position, and "_"
-            for each number order guessed correctly but in the wrong position.
+            matches the class' "right_number" variable, and if not, return an
+            "X" for each digit guessed correctly and in the right position,
+            and "_" for each number order guessed correctly but in the wrong
+            position.
         """
 
-        if number is None or len(number) != 4 or not number.isnumeric():
-            return False
-        if number == RIGHT_NUMBER:
+        if number is None or len(number) != 4 or not number.isdecimal():
+            raise TypeError
+        if number == self.right_number:
             return True
 
         ok_number_ok_position = ''
         ok_number_bad_position = ''
+        found_digits = [False for i in range(10)]
+
+        for digit in number:
+            if found_digits[int(digit)]:
+                raise ValueError
+            found_digits[int(digit)] = True
 
         number = list(number)
 
         for index, digit in enumerate(number):
-            if RIGHT_NUMBER[index] == number[index]:
+            if self.right_number[index] == number[index]:
                 ok_number_ok_position += 'X'
-            elif digit in RIGHT_NUMBER:
+            elif digit in self.right_number:
                 ok_number_bad_position += '_'
 
         return ok_number_ok_position + ok_number_bad_position
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """
     This module will import the Codebreaker module and ask the user to enter a
-    4 digit number. It will give the user "GUESS_LIMIT" amount of tries to
-    guess it.
+    4 digit number.
 """
 
 from codebreaker import Codebreaker
@@ -16,15 +15,17 @@ while True:
     number = input('Try to guess the 4 digit number (enter \'q\' to quit): ')
     if number == 'q':
         break
-    result = codebreaker.guess(number)
-    if not isinstance(result, str):
-        if result:
-            print('You won! Number of tries:', guess_number)
-            break
-        print('Please enter a 4 digit number!')
-        print(f'You\'ve tried {guess_number} times.')
+    try:
+        result = codebreaker.guess(number)
+    except ValueError:
+        print("The number can't contain repeated digits!.")
+    except TypeError:
+        print("Please enter a 4 digit number!.")
     else:
-        print(f'Incorrect! You\'ve tried {guess_number} times.')
+        if not isinstance(result, str) and result:
+            print('You won! Number of guesses:', guess_number)
+            break
+        print(f'Incorrect! You\'ve guessed {guess_number} times.')
         if len(result) > 0:
             print(f'Hint: {result}')
-    guess_number += 1
+        guess_number += 1

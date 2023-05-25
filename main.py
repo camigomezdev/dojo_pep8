2e8c2f59b6b216a435bac0f9b6cae55c12c4446a
ElRode14 <64279782+ElRode14@users.noreply.github.com>
2023-05-25 14:17:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from codebreaker import Codebreaker
 
-intentos_totales = 10
-codebreaker = Codebreaker()
-
+INTENTOS_TOTALES = 10
 intento = 0
+codebreaker = Codebreaker()
 
 print('Jugar Codebreaker!')
 
-while intento != intentos_totales:
-   number = input('Numero:');
-   resolve = codebreaker.adivinar(number)
-   print(resolve)
-   if resolve == True:
-      print('You win!!')
-      break
+while intento != INTENTOS_TOTALES:
+    number = input('Numero: ')
+    intento, resolve = codebreaker.adivinar(intento, number)
 
+    if isinstance(resolve, bool) and resolve:
+        print('You win!!')
+        break
+    elif isinstance(resolve, bool) and not resolve:
+        print('Los números a adivinar no cumplen las condiciones\
+               \nModificar código')
+        break
+    else:
+        print(resolve)
+        print(f'Intentos: {intento} de {INTENTOS_TOTALES}')
 
+if intento == 10:
+    print('No lo has logrado :(')

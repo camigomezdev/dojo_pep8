b90a29f26e2792bac118cc29e03cb2706847922b
sryvcr <andresgx9@gmail.com>
2023-05-25 15:28:15 -0500

    PEP8 applied to main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from codebreaker import Codebreaker
 
-intentos_totales = 10
+
+TOTAL_ATTEMPS = 10
+
 codebreaker = Codebreaker()
 
-intento = 0
+attemp = 0
 
-print('Jugar Codebreaker!')
+print("Let's play Codebreaker!!")
 
-while intento != intentos_totales:
-   number = input('Numero:');
-   resolve = codebreaker.adivinar(number)
-   print(resolve)
-   if resolve == True:
-      print('You win!!')
-      break
+while attemp != TOTAL_ATTEMPS:
+    number = input("Number:")
+    resolve = codebreaker.guess(number)
+    print(resolve)
 
-
+    if resolve:
+        print("You win!!")
+        break
